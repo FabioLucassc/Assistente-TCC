ea82453d9a0f0f98b53b7919971a06b16967db97
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-09-21 11:20:51 -0300

    Testes com Vosk

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import speech_recognition as sr
+#!/usr/bin/env python3
+import pyaudio
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
+import os
+import wave
 
-# Reconhecedor de voz
-reconhecedor = sr.Recognizer()
 
-# Utilizar o microfone para capturar audio
-with sr.Microphone() as source:
+CHANNELS = 2
+RATE = 44100
+CHUNK = 4096
+FORMAT = pyaudio.paInt16
 
-    while True:
-        audio = reconhecedor.listen(source)
-        print(reconhecedor.recognize_google(audio, language='pt'))
+
+model = Model("model")
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+#sample_size = pyaudio.get_sample_size(FORMAT)
+stream.start_stream()
+
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
