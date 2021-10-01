b59841fdcc5747fb4a69ba56cca0d3232ac8dc31
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-10-01 16:22:00 -0300

    Tensorflow

--- pln/modelo.py
+++ pln/modelo.py
@@ -1,5 +1,10 @@
 import yaml
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.utils import to_categorical
 
 
 dados = yaml.safe_load(open('treinamento.yml', 'r', encoding='utf-8').read())
@@ -9,8 +14,7 @@ for comando in dados['comandos']:
     entradas.append(comando['entrada'].lower())
     saidas.append('{}\{}'.format(comando['grupo'], comando['acao']))
 
-# Processamento de texto: palavras, caracteres, bytes, sub-palavras
-
+# Processamento de texto: palavras, caracteres, bytes, sub --------------------------------------------------------------
 chars = set()
 
 for input in entradas + saidas:
@@ -18,11 +22,11 @@ for input in entradas + saidas:
         if ch not in chars:
             chars.add(ch)
 print('',len(chars))
+# Processamento de texto: palavras, caracteres, bytes, sub-palavras -------------------------------------------------------------
 
 
 
-# Mapear char-idx
-
+# Mapear char-idx -------------------------------------------------------------
 caractere_index = {}
 index_caractere = {}
 
@@ -30,20 +34,41 @@ for i, ch in enumerate(chars):
     caractere_index[ch] = i
     index_caractere[i] = ch
 
-
 maior_sequencia = max([len(x) for x in entradas])
 
 print('Número de chars:', len(chars))
 print('Maior seq:', maior_sequencia)
+# Mapear char-idx -------------------------------------------------------------
+
 
 # Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
 # Criar dataset disperso (número de examplos, tamanho da seq)
 
-# Input Data one-hot encoding
+# Input Data one-hot encoding -----------------------------------------------------------------------
 
 dados_entrada = np.zeros((len(entradas), maior_sequencia, len(chars)), dtype='int32')
-for i, entrada in enumerate(entradas):
-    for k, ch in enumerate(entrada):
+
+for i, input in enumerate(entradas):
+    for k, ch in enumerate(input):
         dados_entrada[i, k, caractere_index[ch]] = 1.0
 
-print(dados_entrada)
+# Input Data one-hot encoding -----------------------------------------------------------------------
+
+# Output Data ------------------------------------------------------------------------
+labels = set(saidas)
+
+label_index = {}
+index_label = {}
+
+for k, label in enumerate(labels):
+    label_index[label] = k
+    index_label[k] = label
+
+dados_saida = []
+
+for saida in saidas:
+    dados_saida.append(label_index[saida])
+
+output_data = to_categorical(dados_saida, len(dados_saida))
+
+print(output_data[0])
