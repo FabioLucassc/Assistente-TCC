ce9f19ed256d29d410c765e4f2cae63eb59e4538
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-10-26 23:14:27 -0300

    bytes

--- pln/modelo.py
+++ pln/modelo.py
@@ -14,45 +14,24 @@ for comando in dados['comandos']:
     entradas.append(comando['entrada'].lower())
     saidas.append('{}\{}'.format(comando['grupo'], comando['acao']))
 
-# Processamento de texto: palavras, caracteres, bytes, sub --------------------------------------------------------------
-chars = set()
-
-for input in entradas + saidas:
-    for ch in input:
-        if ch not in chars:
-            chars.add(ch)
-print('',len(chars))
-# Processamento de texto: palavras, caracteres, bytes, sub-palavras -------------------------------------------------------------
-
-
-
-# Mapear char-idx -------------------------------------------------------------
-caractere_index = {}
-index_caractere = {}
-
-for i, ch in enumerate(chars):
-    caractere_index[ch] = i
-    index_caractere[i] = ch
-
-maior_sequencia = max([len(x) for x in entradas])
-
-print('Número de chars:', len(chars))
+# Processamento de texto: palavras, caracteres, bytes, sub ---------------------------------------------------------------------
+maior_sequencia = max([len(bytes(x.encode('utf-8'))) for x in entradas])
 print('Maior seq:', maior_sequencia)
-# Mapear char-idx -------------------------------------------------------------
 
-
-# Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
-# Criar dataset disperso (número de examplos, tamanho da seq)
+# Processamento de texto: palavras, caracteres, bytes, sub-palavras -------------------------------------------------------------
 
 # Input Data one-hot encoding -----------------------------------------------------------------------
 
-dados_entrada = np.zeros((len(entradas), maior_sequencia, len(chars)), dtype='int32')
+dados_entrada = np.zeros((len(entradas), maior_sequencia, 256), dtype='int32')
 
 for i, entrada in enumerate(entradas):
     for k, ch in enumerate(bytes(entrada.encode('utf-8'))):
-        dados_entrada[i, k, caractere_index[ch]] = 1.0
+        dados_entrada[i, k, int[ch]] = 1.0
 
 # Input Data one-hot encoding -----------------------------------------------------------------------
+
+# Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
+# Criar dataset disperso (número de examplos, tamanho da seq)
 
 # Output Data ------------------------------------------------------------------------
 labels = set(saidas)
@@ -72,3 +51,29 @@ for saida in saidas:
 output_data = to_categorical(dados_saida, len(dados_saida))
 
 print(output_data[0])
+
+model = Sequential()
+model.add(LSTM(128))
+model.add(Dense(len(output_data), activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+
+model.fit(dados_entrada, dados_saida, epochs=128)
+
+# Classificar texto em um entidade
+def classificar(text):
+    # Criar um array de entrada
+    x = np.zeros((1, 48, 256), dtype='float32')
+
+    # Preencher o array com dados do texto.
+    for k, ch in enumerate(bytes(text.encode('utf-8'))):
+        x[0, k, int(ch)] = 1.0
+
+    # Fazer a previsão
+    out = model.predict(x)
+    idx = out.argmax()
+    print(index_label[idx])
+
+while True:
+    text = input('Digite algo: ')
+    classificar(text)
