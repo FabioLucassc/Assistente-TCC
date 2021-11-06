80964f25deaf8b104c3abb73d69397b920858cb5
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-11-05 22:14:08 -0300

    treinamento

--- pln/modelo.py
+++ pln/modelo.py
@@ -2,66 +2,74 @@ import yaml
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
 from tensorflow.keras.utils import to_categorical
 
-
 dados = yaml.safe_load(open('treinamento.yml', 'r', encoding='utf-8').read())
+
 entradas, saidas = [], []
 
 for comando in dados['comandos']:
     entradas.append(comando['entrada'].lower())
-    saidas.append('{}\{}'.format(comando['grupo'], comando['acao']))
+    saidas.append('{}\{}'.format(comando['acao'], comando['acao']))
 
-# Processamento de texto: palavras, caracteres, bytes, sub ---------------------------------------------------------------------
+
+# Processar texto: palavras, caracteres, bytes, sub-palavras
+
 maior_sequencia = max([len(bytes(x.encode('utf-8'))) for x in entradas])
+
 print('Maior seq:', maior_sequencia)
 
-# Processamento de texto: palavras, caracteres, bytes, sub-palavras -------------------------------------------------------------
+# Criar dadosset one-hot (número de examplos, tamanho da seq, num caracteres)
+# Criar dadosset disperso (número de examplos, tamanho da seq)
 
-# Input Data one-hot encoding -----------------------------------------------------------------------
+# Input dados one-hot encoding
 
-dados_entrada = np.zeros((len(entradas), maior_sequencia, 256), dtype='int32')
+dados_entrada = np.zeros((len(entradas), maior_sequencia, 256), dtype='float32')
+for i, inp in enumerate(entradas):
+    for k, ch in enumerate(bytes(inp.encode('utf-8'))):
+        dados_entrada[i, k, int(ch)] = 1.0
 
-for i, entrada in enumerate(entradas):
-    for k, ch in enumerate(bytes(entrada.encode('utf-8'))):
-        dados_entrada[i, k, int[ch]] = 1.0
 
-# Input Data one-hot encoding -----------------------------------------------------------------------
+# Input dados sparse
+'''
+dados_entrada = np.zeros((len(entradas), maior_sequencia), dtype='int32')
+for i, input in enumerate(entradas):
+    for k, ch in enumerate(input):
+        dados_entrada[i, k] = chr2idx[ch]
+'''
 
-# Criar dataset one-hot (número de examplos, tamanho da seq, num caracteres)
-# Criar dataset disperso (número de examplos, tamanho da seq)
+# Output dados
 
-# Output Data ------------------------------------------------------------------------
 labels = set(saidas)
 
-label_index = {}
-index_label = {}
+label2idx = {}
+idx2label = {}
 
 for k, label in enumerate(labels):
-    label_index[label] = k
-    index_label[k] = label
+    label2idx[label] = k
+    idx2label[k] = label
 
-dados_saida = []
+saida_dados = []
 
-for saida in saidas:
-    dados_saida.append(label_index[saida])
+for output in saidas:
+    saida_dados.append(label2idx[output])
 
-output_data = to_categorical(dados_saida, len(dados_saida))
+saida_dados = to_categorical(saida_dados, len(saida_dados))
 
-print(output_data[0])
+
+print(saida_dados[0])
 
 model = Sequential()
 model.add(LSTM(128))
-model.add(Dense(len(output_data), activation='softmax'))
+model.add(Dense(len(saida_dados), activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
-model.fit(dados_entrada, dados_saida, epochs=128)
+model.fit(dados_entrada, saida_dados, epochs=128)
 
 # Classificar texto em um entidade
-def classificar(text):
+def classify(text):
     # Criar um array de entrada
     x = np.zeros((1, 48, 256), dtype='float32')
 
@@ -72,8 +80,8 @@ def classificar(text):
     # Fazer a previsão
     out = model.predict(x)
     idx = out.argmax()
-    print(index_label[idx])
+    print(idx2label[idx])
 
 while True:
     text = input('Digite algo: ')
-    classificar(text)
+    classify(text)
