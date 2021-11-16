4a2813ff7c321b6994d8997b1ae22dd9ad21cd08
fabiolucassc <fabio-lucas1.000@hotmail.com>
2021-11-15 21:17:29 -0300

    Update teste.py

--- test/teste.py
+++ test/teste.py
@@ -1,35 +1,9 @@
-from tensorflow.keras.models import load_model
-import numpy as np
+import string
+import urllib.request, json
+import re
 
-# modelo = load_model('pln\modelo.py')
-modelo = load_model('model.h5')
-
-labels = open('labels.txt', 'r', encoding='utf-8').read().split('\n')
-
-transformar_label2index = {}
-transformar_index2label = {}
-
-for k, label in enumerate(labels):
-    transformar_label2index[label] = k
-    transformar_index2label[k] = label
-
-
-# Classificar texto em um entidade
-def classificar(text):
-    # Criar um array de entrada
-    x = np.zeros((1, 48, 256), dtype='float32')
-
-    # Preencher o array com dados do texto.
-    for k, ch in enumerate(bytes(text.encode('utf-8'))):
-        x[0, k, int(ch)] = 1.0
-
-    # Fazer a previsão
-    out = modelo.predict(x)
-    score = modelo.score()
-    print(score)
-    idx = out.argmax()
-    return transformar_index2label[idx]
-
-while True:
-    text = input('Digite algo: ')
-    print(classificar(text))
+with urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=jundiai&units=metric&appid=7bad02a3959dce28b73e516b02972c73&lang=pt_br") as url:
+    data = json.loads(url.read().decode())
+print(data)
+print(data['main']['temp'])
+print(data['weather'])
