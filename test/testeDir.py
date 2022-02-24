8a79138205b6a7f055b5f8c8f41635489db5b46c
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2022-02-24 19:00:45 -0300

    teste diretorio manual

--- test/testeDir.py
+++ test/testeDir.py
@@ -5,10 +5,22 @@ from os import path
 from speech_recognition import Microphone
 
 caminho = path.join(path.expanduser("~"))
-print(caminho)
+# print(caminho)
 diretorio = "C:"
-for teste in os.listdir(diretorio):
-    print(teste)
-print(os.listdir(diretorio))
+# for teste in os.listdir(diretorio):
+#     print(teste)
+# print(os.listdir(diretorio))
 
-print(diretorio)
+# print(diretorio)
+palavra = ""
+diretorio1 =""
+# print(os.listdir(diretorio1))
+while(True):
+   diretorio1 += palavra
+   palavra = input('Diretorio: ')
+   if(palavra == "D:"):
+       palavra = ""
+       diretorio1 = "D:\\"
+       print(os.listdir(diretorio1))
+   else:
+     break
