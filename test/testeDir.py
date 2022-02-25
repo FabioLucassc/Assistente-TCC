dc676fc8faf36b96d29e609680a3df6c17fff3b3
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2022-02-24 21:00:27 -0300

    Update testeDir.py

--- test/testeDir.py
+++ test/testeDir.py
@@ -1,26 +1,23 @@
 import os
-import subprocess
 from os import path
 
-from speech_recognition import Microphone
-
 caminho = path.join(path.expanduser("~"))
-# print(caminho)
-diretorio = "C:"
-# for teste in os.listdir(diretorio):
-#     print(teste)
-# print(os.listdir(diretorio))
 
-# print(diretorio)
-palavra = ""
-diretorio1 ="C:"
-# print(os.listdir(diretorio1))
-while(True):
-   palavra = input('Diretorio: ')
-   if(palavra == "D:"):
-       palavra = ""
-       diretorio1 = "D:\\"
-       print(os.listdir(diretorio1))
-   else:
-       diretorio1 += "\\"+palavra
-       print(os.listdir(diretorio1))
+diretorio = ""
+
+while (True):
+    palavra = input('Diretorio: ')
+    if (palavra == "D:"):
+        palavra = ""
+        diretorio = "D:\\"
+        print(os.listdir(diretorio))
+        while (True):
+            try:
+                palavra = input("Qual diretorio ?\n"+str(os.listdir(diretorio))+"\n")
+                diretorio += "\\" + palavra
+            except:
+                print("diretorio não encontrado")
+                break 
+    else:
+        diretorio += "\\" + palavra
+        print(os.listdir(diretorio))
