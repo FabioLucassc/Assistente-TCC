8f7ea838587315a2924274a067b644ca748ef776
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2022-02-27 08:48:50 -0300

    Teste diretorios

--- test/testeDir.py
+++ test/testeDir.py
@@ -2,23 +2,51 @@ import os
 from os import path
 
 caminho = path.join(path.expanduser("~"))
+# print(str(os.listdir("D:\\temp\\teste")))
 
 diretorio = ""
+diretorio_anterior = ""
 
 while (True):
     palavra = input('Diretorio inicial: ')
     if (palavra == "D:"):
+
         palavra = ""
         diretorio = "D:\\"
-        print(os.listdir(diretorio))
+        diretorio_anterior = diretorio
+
         while (True):
+
+            diretorio_anterior = diretorio
+
             try:
+
                 palavra = input("Qual diretorio ?\n"+str(os.listdir(diretorio))+"\n")
                 diretorio += "\\" + palavra
-            except:
-                print("diretorio não encontrado")
-                print(diretorio)
+                os.listdir(diretorio)
 
-    else:
-        diretorio += "\\" + palavra
-        print(os.listdir(diretorio))
+            except:
+
+                print("diretorio não encontrado")
+                diretorio = diretorio_anterior
+
+    elif (palavra == "C:"):
+
+        palavra = ""
+        diretorio = "C:\\"
+        diretorio_anterior = diretorio
+
+        while (True):
+
+            diretorio_anterior = diretorio
+
+            try:
+
+                palavra = input("Qual diretorio ?\n" + str(os.listdir(diretorio)) + "\n")
+                diretorio += "\\" + palavra
+                os.listdir(diretorio)
+
+            except:
+
+                print("diretorio não encontrado")
+                diretorio = diretorio_anterior
