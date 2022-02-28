ca6c425a77edfad41bece2b7eb80fd0659383b70
brandoov <vitor.brsantos@outlook.com>
2022-02-27 23:04:44 -0300

    Classe Microfone

--- google_main.py
+++ google_main.py
@@ -1,8 +1,25 @@
 import pyttsx3
 import speech_recognition as sr
 from unidecode import unidecode
+from os import path
+import subprocess
+import os
+import Microfone as m
 
 import comandos
+
+mic = m.cMicrofone()
+
+caminho = ''
+diretorio = 'D:\\'
+
+while diretorio != "navegar":
+    caminho += diretorio + "\\"
+    diretorio = mic.Ouvir()
+    print(diretorio)
+
+print(caminho)
+subprocess.Popen(f'explorer "{caminho}"')
 
 reconhecedor = sr.Recognizer()
 
@@ -20,8 +37,25 @@ with sr.Microphone() as source:
 
                 resultado = unidecode(resultado)
 
+                palavras = ""
+
+                for palavra in resultado.split(" "):
+                    palavras += palavra + "\\"
+
+                disco = path.splitdrive(os.getcwd())[0]
+                caminho = ""
+
+                if (disco.__contains__("C")):
+                    caminho = path.join(path.expanduser("~"), palavras)
+                else:
+                    caminho = f"{disco}\\{palavras}"
+
+                #path.splitdrive(os.getcwd())[0]
+                #os.startfile(path.relpath(caminho))
+                subprocess.Popen(f'explorer "{caminho}"')
+
                 if resultado != "":
                     comandos.Executar.excutar_comandos(resultado)
 
         except Exception as e:
-            print((type(e).__name__ + ': ' + str(e)))
+            print((type(e).__name__ + ': ' + str(e.__str__())))
