3dec223ee8dfef9b06bfe7ed596d6bab8c6557c0
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2022-03-02 10:12:32 -0300

    Teste manipulação de disco genericamente

--- test/testeDir.py
+++ test/testeDir.py
@@ -1,46 +1,78 @@
 import os
+import string
 from os import path
 
+alfabeto = list(string.ascii_lowercase)
 caminho = path.join(path.expanduser("~"))
 # print(str(os.listdir("D:\\temp\\teste")))
 
 diretorio = ""
 diretorio_anterior = ""
 
+# while (True):
+#
+#     palavra = input('Diretorio inicial: ')
+#
+#     if (palavra == "D:"):
+#
+#         palavra = ""
+#         diretorio = "D:\\"
+#         diretorio_anterior = diretorio
+#
+#         while (True):
+#
+#             diretorio_anterior = diretorio
+#
+#             try:
+#
+#                 palavra = input("Qual diretorio ?\n"+str(os.listdir(diretorio))+"\n")
+#                 diretorio += "\\" + palavra
+#                 os.listdir(diretorio)
+#
+#             except:
+#
+#                 print("diretorio não encontrado")
+#                 diretorio = diretorio_anterior
+#
+#     elif (palavra == "C:"):
+#
+#         palavra = ""
+#         diretorio = "C:\\"
+#         diretorio_anterior = diretorio
+#
+#         while (True):
+#
+#             diretorio_anterior = diretorio
+#
+#             try:
+#
+#                 palavra = input("Qual diretorio ?\n" + str(os.listdir(diretorio)) + "\n")
+#                 diretorio += "\\" + palavra
+#                 os.listdir(diretorio)
+#
+#             except:
+#
+#                 print("diretorio não encontrado")
+#                 diretorio = diretorio_anterior
+#     elif (palavra in a):
+#         print("Teste")
+#     else:
+#         print("Disco não encontrado")
+
 while (True):
 
-    palavra = input('Diretorio inicial: ')
+    palavra = input('Qual disco inicial: ')
 
-    if (palavra == "D:"):
-
-        palavra = ""
-        diretorio = "D:\\"
+    if (palavra.lower() in alfabeto):
+        # palavra = ""
+        # diretorio = "C:\\"
+        diretorio = palavra.upper()+":\\"
         diretorio_anterior = diretorio
 
         while (True):
 
             diretorio_anterior = diretorio
 
-            try:
-
-                palavra = input("Qual diretorio ?\n"+str(os.listdir(diretorio))+"\n")
-                diretorio += "\\" + palavra
-                os.listdir(diretorio)
-
-            except:
-
-                print("diretorio não encontrado")
-                diretorio = diretorio_anterior
-
-    elif (palavra == "C:"):
-
-        palavra = ""
-        diretorio = "C:\\"
-        diretorio_anterior = diretorio
-
-        while (True):
-
-            diretorio_anterior = diretorio
 
             try:
 
@@ -49,8 +81,10 @@ while (True):
                 os.listdir(diretorio)
 
             except:
-
                 print("diretorio não encontrado")
+                if (diretorio == palavra.upper() + ":\\"):
+                    break
                 diretorio = diretorio_anterior
+
     else:
-        print("Disco não encontrado")
+        print("Por favor selecione um disco valido!")
