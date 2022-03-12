41ca1f467239bc348613dfaf86d5db5852c46053
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2022-03-12 14:45:01 -0300

    Update teste.py

--- test/teste.py
+++ test/teste.py
@@ -2,8 +2,9 @@ import string
 import urllib.request, json
 import re
 
-with urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=jundiai&units=metric&appid=7bad02a3959dce28b73e516b02972c73&lang=pt_br") as url:
-    data = json.loads(url.read().decode())
-print(data)
-print(data['main']['temp'])
-print(data['weather'])
+# with urllib.request.urlopen("https://api.openweathermap.org/data/2.5/weather?q=jundiai&units=metric&appid=7bad02a3959dce28b73e516b02972c73&lang=pt_br") as url:
+#     data = json.loads(url.read().decode())
+# print(data)
+# print(data['main']['temp'])
+# print(data['weather'])
+
